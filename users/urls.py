89d85c04423c54afb5567dc8f7d19399d2b0448a
Berkehan <b2200356072@cs.hacettepe.edu.tr>
2024-07-03 13:32:20 +0300

    subscription button add

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import register, user_login, logout_view
+from .views import register, user_login, logout_view, activate_subscribe, deactivate_subscribe
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('activate-subscribe/', activate_subscribe, name='activate_subscribe'),
+    path('deactivate-subscribe/', deactivate_subscribe, name='deactivate_subscribe'),
 ]
