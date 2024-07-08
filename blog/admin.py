4dbea6b17f88cd08cd0561d2cfbfeb93d10b3c94
Berkehan <b2200356072@cs.hacettepe.edu.tr>
2024-07-08 12:18:18 +0300

    subscriber mail system updated

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from .models import BlogPost, Category
 from django.utils.timezone import now
-
+from .views import send_newpost_email_to_subscribers
+from django.db.models import Q
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'category', 'created_at', 'isPublished', 'publish_date')
@@ -14,6 +15,11 @@ class BlogPostAdmin(admin.ModelAdmin):
 
     def make_published(self, request, queryset):
         count = queryset.update(isPublished=True)
+        for post in queryset.filter(Q(publish_date__isnull=True)):
+            try:
+                send_newpost_email_to_subscribers(post.slug)
+            except:
+                pass # TODO logging
         queryset.update(publish_date=now())
         self.message_user(request, f'{count} blog posts have been published.')
     make_published.short_description = 'Publish selected blog posts'
